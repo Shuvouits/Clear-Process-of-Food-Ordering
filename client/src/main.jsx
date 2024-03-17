3f7dd8a1f107920a6e0b6a4f165dd6e1410b7f0d
Shuvo Bhowmik <shuvobhowmik_cse11uits@yahoo.com>
2024-03-17 15:06:36 +0600

    Git hub issue

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {BrowserRouter as Router} from 'react-router-dom'
+import {createStore} from 'redux'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
